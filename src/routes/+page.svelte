1daa3f706c69556944633b617c0f0d252ae73d68
Sean Griffin <sgriffin6697@gmail.com>
2024-05-24 12:43:19 -0700

    adding charts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Chart, Svg, Axis, Area } from 'layerchart';
+	import { format, Field, Switch, Toggle, PeriodType } from 'svelte-ux';
+	import { scaleTime } from 'd3-scale';
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<div class="h-[300px] rounded border p-4">
+	<Chart
+		data={data.dateSeriesData}
+		x="date"
+		xScale={scaleTime()}
+		y="value"
+		yDomain={[0, null]}
+		yNice
+		padding={{ left: 16, bottom: 24 }}
+	>
+		<Svg>
+			<Axis placement="left" grid rule />
+			<Axis
+				placement="bottom"
+				format={(d) => format(d, PeriodType.Day, { variant: 'short' })}
+				rule
+			/>
+			<Area line={{ class: 'stroke-2 stroke-primary' }} class="fill-primary/40" />
+		</Svg>
+	</Chart>
+</div>
