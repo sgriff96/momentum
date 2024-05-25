b651ff429f76ef9682a10168674e9b757ce74c68
Sean Griffin <sgriffin6697@gmail.com>
2024-05-24 18:50:24 -0700

    update nav

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a {href} class="group transition duration-300">
+	<slot />
+	<span class="block h-0.5 max-w-0 bg-primary transition-all duration-300 group-hover:max-w-full"
+	></span>
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,25 @@
 
 	import { toggleMode } from 'mode-watcher';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import NavItem from '$lib/components/NavItem.svelte';
 </script>
 
 <ModeWatcher />
-<Button on:click={toggleMode} variant="outline" size="icon">
-	<Sun
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Moon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
-	<span class="sr-only">Toggle theme</span>
-</Button>
-<slot />
+
+<div class="mx-2">
+	<div class="mt-2 flex items-center justify-between rounded-md border border-border px-4 py-2">
+		<div class="flex gap-4">
+			<NavItem href="/">Home</NavItem>
+		</div>
+		<Button on:click={toggleMode} variant="outline" size="icon">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</div>
+	<slot />
+</div>
