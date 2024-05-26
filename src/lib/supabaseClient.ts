e78a49d2246b0c66461bb98e7db8322de91cc15f
Sean Griffin <sgriffin6697@gmail.com>
2024-05-25 21:19:49 -0700

    cloudflare

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 import { VITE_SB_PROJECT_URL, VITE_SB_ANON_KEY } from '$env/static/private';
-import type { Database, Tables } from './types/supabase';
+import type { Database } from './types/supabase';
 
 export const supabase = createClient<Database>(VITE_SB_PROJECT_URL, VITE_SB_ANON_KEY);
-
-let habits: Tables<'habits'>[];
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/adapter-vercel).Config} */
@@ -12,8 +12,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-	runtime: 'edge'
+	}
 };
 
 export default config;
