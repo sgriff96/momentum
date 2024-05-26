d0c9658c70762e1f18b80691e891ecfe6274ada1
Sean Griffin <sgriffin6697@gmail.com>
2024-05-25 21:25:38 -0700

    cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/adapter-vercel).Config} */
+/** @type {import('@sveltejs/adapter-cloudflare).Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
