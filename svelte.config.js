02267814d47f4d33f9e489bd5e746d2af11f123f
Sean Griffin <sgriffin6697@gmail.com>
2024-06-06 18:08:57 +0000

    supress node modules warnings

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/adapter-cloudflare).Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
+	onwarn: (warning, handler) => {
+		if (warning.filename.includes('node_modules')) return;
+		handler(warning);
+	},
 	kit: {
 		adapter: adapter(),
 	},
