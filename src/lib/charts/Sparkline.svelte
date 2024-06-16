e851fb853b03db9420eec28d6f87f3497c9bf97c
Sean Griffin <sgriffin6697@gmail.com>
2024-06-16 04:16:05 +0000

    no data message

--- src/lib/charts/Sparkline.svelte
+++ src/lib/charts/Sparkline.svelte
@@ -13,6 +13,12 @@
 	}));
 </script>
 
+{#if data.length === 0}
+	<div class="flex justify-center">
+		<p class="text-sm text-muted-foreground">No data</p>
+	</div>
+{/if}
+
 <div class="h-[25px]">
 	<Chart data={newData} x="date" xScale={scaleTime()} y="value" tooltip={{ mode: 'bisect-x' }}>
 		<Svg>
