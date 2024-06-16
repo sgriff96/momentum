9d8515512836fe3cfde672fdaf4d60ea12a2c81d
Sean Griffin <sgriffin6697@gmail.com>
2024-06-16 05:11:00 +0000

    update sparkline

--- src/lib/charts/Sparkline.svelte
+++ src/lib/charts/Sparkline.svelte
@@ -13,18 +13,18 @@
 	}));
 </script>
 
-{#if data.length === 0}
-	<div class="flex justify-center">
-		<p class="text-sm text-muted-foreground">No data</p>
-	</div>
-{/if}
-
 <div class="h-[25px]">
-	<Chart data={newData} x="date" xScale={scaleTime()} y="value" tooltip={{ mode: 'bisect-x' }}>
-		<Svg>
-			<Spline class="stroke-primary stroke-1" />
-			<Highlight points={{ r: 3, class: 'stroke-none' }} />
-		</Svg>
-		<Tooltip class="text-xs" contained={false} header={(data) => format(data.date, 'yyyy-MM-dd')} />
-	</Chart>
+	{#if data.length === 0}
+		<div class="flex justify-center">
+			<p class="text-sm text-muted-foreground">No data</p>
+		</div>
+	{:else}
+		<Chart data={newData} x="date" xScale={scaleTime()} y="value" tooltip={{ mode: 'bisect-x' }}>
+			<Svg>
+				<Spline class="stroke-primary stroke-1" />
+				<Highlight points={{ r: 3, class: 'stroke-none' }} />
+			</Svg>
+			<Tooltip class="text-xs" contained={false} header={(data) => format(data.date, 'yyyy-MM-dd')} />
+		</Chart>
+	{/if}
 </div>
